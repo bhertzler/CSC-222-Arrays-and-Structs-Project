85f7272769a7255a0f2d13caa8afe0819a7d12af
Ben Hertzler <bwhertzler@outlook.com>
2024-10-10 19:13:49 -0400

    Add project files.

---
+++ CSC 222 Arrays and Structs Project.cpp
@@ -0,0 +1,15 @@
+// CSC 222 Arrays and Structs Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// Benjamin Hertzler
+// Arrays and Structs Programming Projects
+// Master
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
